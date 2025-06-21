6497cdab97a5acab2fcf5c3e15ffee36229a03b3
Vignesh8205 <115607497+Vignesh8205@users.noreply.github.com>
2025-06-21 15:41:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 console.log("Hello, World!");
 console.log("Hello, World!");
-console.log("HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello, World!");
+console.log("'piouilkuyjtdfygkuihlj;ok'l;kjlkjhg, World!");jh
 console.log("Hello, World!");
